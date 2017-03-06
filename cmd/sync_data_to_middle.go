ea2816d2367989b2ad99eac8dee799eda8c76bd4
Anthony Alaribe <anthonyalaribe@gmail.com>
2017-03-06 10:07:27 +0100

    moved trigger and related sql queries to functions in queries package

--- cmd/sync_data_to_middle.go
+++ cmd/sync_data_to_middle.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql" //A mysql driver to allow database/sql understand the database
@@ -12,135 +11,46 @@ import (
 	client "gitlab.com/middlefront/go-middle-client"
 	"gitlab.com/middlefront/middle/core"
 	"gitlab.com/middlefront/middle/props"
+
+	"gitlab.com/middlefront/sqldb-provider/queries"
 )
-
-//getTables returns all tables in a given database, and an error, if unsuccessful
-func getTables(db *sql.DB) ([]string, error) {
-	var tables []string
-	rows, err := db.Query("show TABLES")
-	if err != nil {
-		log.Fatalf("unable to get tables from database. Error: %+v", err.Error())
-		return tables, err
-	}
-
-	var tablename string
-	for rows.Next() {
-		err = rows.Scan(&tablename)
-		if err != nil { /* error handling. Not sure what kind of errors would return nil when rows.Next() had returned true. TODO: handle error appropriately */
-			log.Fatalf("unable to get tables from database. Error: %+v", err.Error())
-		}
-		tables = append(tables, tablename)
-	}
-
-	return tables, err
-}
 
 func createTriggers(db *sql.DB) {
 	dbName := viper.GetString(databaseName)
-	var query string
 
-	rows, err := db.Query("SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLUMN_DEFAULT, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '" + dbName + "';")
-	if err != nil {
-		log.Fatalf("unable to get colums in tables. Error: %+v", err.Error())
-	}
-
-	var data = struct {
-		TableSchema   string
-		TableName     string
-		ColumnName    string
-		ColumnDefault string
-		DataType      string
-	}{}
-
-	tablesAndColumns := map[string][]string{}
-
-	for rows.Next() {
-		err = rows.Scan(&data.TableSchema, &data.TableName, &data.ColumnName, &data.ColumnDefault, &data.DataType)
-
-		columns := tablesAndColumns[data.TableName]
-		columns = append(columns, data.ColumnName)
-		tablesAndColumns[data.TableName] = columns
-		if err != nil {
-			/* error handling. Not sure what kind of errors would return nil when rows.Next() had returned true. TODO: handle error appropriately */
-			log.Printf("unable to get tables from database. Error: %+v", err.Error())
-		}
-	}
-
-	log.Println(tablesAndColumns)
-
-	// SELECT KU.table_name as TABLENAME,
-	// column_name as PRIMARYKEYCOLUMN
-	// FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
-	// INNER JOIN
-	// 	INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
-	// 	ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' AND
-	// 	TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME AND
-	// 	KU.table_name='%s'
-	// 	ORDER BY KU.ORDINAL_POSITION;
-
-	tablesAndPrimaryKeys := make(map[string][]string)
-
-	for tablename := range tablesAndColumns {
-		query = fmt.Sprintf(`
-			SELECT column_name as PRIMARYKEYCOLUMN
-			FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
-			INNER JOIN
-				INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
-				ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' AND
-				TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME AND
-				KU.table_name='%s'
-				ORDER BY KU.ORDINAL_POSITION;
-			`, tablename)
-		rows, err = db.Query(query)
-		if err != nil {
-			log.Fatalf("unable to get colums in tables. Error: %+v", err.Error())
-		}
-		var primarykeycolumn string
-		primaryKeysMap := make(map[string]interface{})
-		for rows.Next() {
-			err = rows.Scan(&primarykeycolumn)
-			if err != nil {
-				log.Println(err)
-			}
-			primaryKeysMap[primarykeycolumn] = nil
-		}
-
-		var primaryKeysList []string
-		for primaryKey := range primaryKeysMap {
-			primaryKeysList = append(primaryKeysList, primaryKey)
-		}
-
-		tablesAndPrimaryKeys[tablename] = primaryKeysList
-
-	}
-
-	log.Println(tablesAndPrimaryKeys)
-
-	// TODO: Create a table to store all changes, old and new, alongside the
-	// ID | KEYS (x:xval,y:yval) | TABLE_NAME | TRIGGER_TYPE
-
-	query = fmt.Sprintf(`CREATE TABLE meta_changelog(
-	   ID int NOT NULL AUTO_INCREMENT,
-		 TableName varchar(255),
-		 PrimaryKeys varchar(255),
-		 OldColumnValue varchar(255),
-		 NewColumnValue varchar(255),
-		 TriggerType varchar(10),
-		 ActionDate datetime NOT NULL DEFAULT NOW(),
-	   PRIMARY KEY(ID)
-	);`)
-	_, err = db.Query(query)
+	tablesAndColumns, err := queries.GetAllTablesAndColumns(db, dbName)
 	if err != nil {
 		log.Println(err)
 	}
 
+	//tablesAndPrimaryKeys := make(map[string][]string)
+
+	// for tablename := range tablesAndColumns {
+	// 	primaryKeysList := GetAllPrimaryKeysInTable(db, tablename)
+	// 	tablesAndPrimaryKeys[tablename] = primaryKeysList
+	// }
+
+	//log.Println(tablesAndPrimaryKeys)
+
+	//Create the changelog table where changes will be logged
+	err = queries.CreateMetaChangeLogTable(db)
+	if err != nil {
+		log.Println(err)
+	}
+
+	//loop through all tables to set triggers for each of them.
 	for tablename, columns := range tablesAndColumns {
-		var pkSQLString string
-		log.Println(tablesAndPrimaryKeys[tablename])
-		for _, pks := range tablesAndPrimaryKeys[tablename] {
-			pkSQLString += "'" + pks + ":',OLD." + pks + ",',',"
+		var primaryKeysAndValues string
+
+		primaryKeysList, err := queries.GetAllPrimaryKeysInTable(db, tablename)
+		if err != nil {
+			log.Println(err)
 		}
-		pkSQLString += "''"
+
+		for _, pks := range primaryKeysList {
+			primaryKeysAndValues += "'" + pks + ":',OLD." + pks + ",',',"
+		}
+		primaryKeysAndValues += "''"
 
 		var OldColumnValues string
 		var NewColumnValues string
@@ -152,48 +62,16 @@ func createTriggers(db *sql.DB) {
 		OldColumnValues += "''"
 		NewColumnValues += "''"
 
-		query = fmt.Sprintf(`
-			DROP TRIGGER %s_delete_trigger;`, tablename)
-		log.Println(query)
-		_, err = db.Query(query)
-		if err != nil {
-			log.Println(err)
-		}
-		query = fmt.Sprintf(`
-			DROP TRIGGER %s_delete_trigger;
-			CREATE TRIGGER %s_delete_trigger
-		AFTER DELETE ON %s
-		FOR EACH ROW
-  	INSERT INTO meta_changelog (TableName, PrimaryKeys, OldColumnValue,  TriggerType )
-      VALUES ('%s',CONCAT(%s),CONCAT(%s),'D');`, tablename, tablename, tablename, tablename, pkSQLString, OldColumnValues)
-
-		log.Println(query)
-		_, err = db.Query(query)
+		err = queries.CreateDeleteTrigger(db, tablename, primaryKeysAndValues, OldColumnValues)
 		if err != nil {
 			log.Println(err)
 		}
 
-		query = fmt.Sprintf(`
-			DROP TRIGGER %s_delete_trigger;`, tablename)
-		log.Println(query)
-		_, err = db.Query(query)
-		if err != nil {
-			log.Println(err)
-		}
-		query = fmt.Sprintf(`
-			DROP TRIGGER %s_update_trigger;
-			CREATE TRIGGER %s_update_trigger
-		AFTER UPDATE ON %s
-		FOR EACH ROW
-		INSERT INTO meta_changelog (TableName, PrimaryKeys, OldColumnValue, NewColumnValue, TriggerType )
-			VALUES ('%s',CONCAT(%s),CONCAT(%s),CONCAT(%s), 'U');`, tablename, tablename, tablename, tablename, pkSQLString, OldColumnValues, NewColumnValues)
-		log.Println(query)
-		_, err = db.Query(query)
+		err = queries.CreateUpdateTrigger(db, tablename, primaryKeysAndValues, OldColumnValues, NewColumnValues)
 		if err != nil {
 			log.Println(err)
 		}
 	}
-
 }
 
 func createTriggersHandler() error {
@@ -234,7 +112,7 @@ func syncDataToMiddle() error {
 		return err
 	}
 
-	tables, err := getTables(db)
+	tables, err := queries.GetAllTables(db)
 	if err != nil {
 		log.Printf("unable to get dataBases. Error: %+v", err.Error())
 		return err
---
+++ queries/triggers.go
@@ -0,0 +1,60 @@
+//queries package exports database access queries, to help decrease clutter in the packages using these queries.
+package queries
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql" //A mysql driver to allow database/sql understand the database
+)
+
+//CreateDeleteTrigger for any given tablename, to store its previous values in the db, and a form of primary key to access the data if need be.
+func CreateDeleteTrigger(db *sql.DB, tablename, primaryKeysAndValues, OldColumnValues string) error {
+	query := fmt.Sprintf(`
+    DROP TRIGGER %s_delete_trigger;`, tablename)
+	_, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+	query = fmt.Sprintf(`
+    CREATE TRIGGER %s_delete_trigger
+      AFTER DELETE ON %s
+      FOR EACH ROW
+        INSERT INTO meta_changelog (TableName, PrimaryKeys, OldColumnValue,  TriggerType )
+        VALUES ('%s',CONCAT(%s),CONCAT(%s),'D');
+    `, tablename, tablename, tablename, primaryKeysAndValues, OldColumnValues)
+
+	log.Println(query)
+	_, err = db.Query(query)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+//CreateUpdateTrigger for any given tablename, to store its previous values in the db, and a form of primary key to access the data if need be, and also the updated data to another column.
+func CreateUpdateTrigger(db *sql.DB, tablename, primaryKeysAndValues, OldColumnValues, NewColumnValues string) error {
+	query := fmt.Sprintf(`
+			DROP TRIGGER %s_delete_trigger;`, tablename)
+	_, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+	query = fmt.Sprintf(`
+			CREATE TRIGGER %s_update_trigger
+		    AFTER UPDATE ON %s
+		    FOR EACH ROW
+		      INSERT INTO meta_changelog (TableName, PrimaryKeys, OldColumnValue, NewColumnValue, TriggerType )
+			    VALUES ('%s',CONCAT(%s),CONCAT(%s),CONCAT(%s), 'U');
+      `, tablename, tablename, tablename, primaryKeysAndValues, OldColumnValues, NewColumnValues)
+
+	log.Println(query)
+	_, err = db.Query(query)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
